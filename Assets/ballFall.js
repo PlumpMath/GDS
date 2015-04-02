d8a851ae3caa49390919ecf1be2442b039455863
dat290 <dat290@nyu.edu>
2015-04-02 18:18:01 -0500

    Yield and Coroutines

---
+++ Assets/ballFall.js
@@ -0,0 +1,18 @@
+﻿var ball : GameObject;
+var readynow : boolean = true;
+
+function Update () {
+
+	if(readynow){
+		MakeBall();
+		
+	}
+	
+}
+
+function MakeBall(){
+	readynow=false;
+	Instantiate(ball, transform.position, transform.rotation);
+	yield WaitForSeconds(1.5);
+	readynow=true;
+}
